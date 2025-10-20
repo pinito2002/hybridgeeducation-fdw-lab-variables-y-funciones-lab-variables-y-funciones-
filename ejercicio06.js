b3f069a0aa63136fcaababf4bd401a8a3cd93bc0
pinito2002 <3dmarketmxai@gmail.com>
2025-10-20 13:24:45 -0600

    Update ejercicio06.js

--- ejercicio06.js
+++ ejercicio06.js
@@ -3,7 +3,9 @@
 
 //escribe tu función aquí. Recuerda que el nombre de la función debe ser exactamente "celsiusAFahrenheit"
 
-
+function celsiusAFahrenheit(celsius) {
+    return (celsius * 9/5) + 32;
+}
 // Exporta la función para que pueda ser utilizada en las pruebas
 module.exports = {
     celsiusAFahrenheit
